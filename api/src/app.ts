87b3d314dd7f24920d2f7877458fab5a7b791b4e
Clara Fang <clara.fang@gmail.com>
2026-06-07 21:46:03 -0400

    initial impl for api calls for regular site

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import { postRoutes } from "./post.js";
 import { tagRoutes } from "./tag.js";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use("/*", cors());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
